b8aec361a31b5350672f0344520aa12aa41dc970
DayaneMoises <Dayane.moises@hotmail.com>
2026-03-11 15:38:57 -0300

    fix: coloca coluna como unique

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -16,8 +16,11 @@ public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String dificuldade;
+
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
 }
