866bc504aae07ede80b7579110eb928455ed7eb8
DayaneMoises <Dayane.moises@hotmail.com>
2026-03-22 11:48:27 -0300

    feat: Cria controller com mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,34 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missao")
 public class MissoesController {
+
+    @GetMapping("/all")
+    public String getAll(){
+        return "Todas as missões";
+    }
+
+    @GetMapping("/byId")
+    public String getbyId(){
+        return "Missões por id";
+    }
+
+    @PostMapping("/create")
+    public String create(){
+        return "Cria missão";
+    }
+
+    @PutMapping("/update")
+    public String update(){
+        return "Atualiza missão";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete(){
+        return "Delete missão";
+    }
+
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,41 +3,33 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninja")
 public class NinjaController {
 
-    @GetMapping("/boasvindas")
-    public String boasVindas(){
-        return "Bem vindo";
-    }
-
-    //Criar ninja
-    @PostMapping("/ninja")
+    @PostMapping("/create")
     public String criarNinja(){
         return "Ninja criado!";
     }
 
      //Buscar ninja por id
-     @GetMapping("/ninja/id")
+     @GetMapping("/byId")
      public String getNinjaId(){
          return "Ninja ID!";
      }
 
      //Buscar todos os ninjas
-     @GetMapping("/ninjas")
+     @GetMapping("/all")
      public String getNinjas(){
          return "Todos os ninjas";
      }
 
      //Alterar dados do ninja
-     @PutMapping("/ninja/atualiza")
+     @PutMapping("/update")
      public String putNinja(){
          return "Ninja atualizado";
      }
 
-     //Deletar ninja
-     //Alterar dados do ninja
-     @DeleteMapping("/ninja/delete")
+     @DeleteMapping("/delete")
      public String deletetNinja(){
          return "Ninja deletado!";
      }
